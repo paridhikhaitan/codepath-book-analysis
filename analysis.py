7a6368328b249e599c9dece07f58147e4c85d47b
paridhikhaitan <paridhi4699@gmail.com>
2020-07-18 16:24:04 +0530

    trie portion q6

--- analysis.py
+++ analysis.py
@@ -1,6 +1,9 @@
 import string
 from collections import Counter
+from collections import defaultdict
 import heapq
+import random
+import re
 
 class LittleWomen:
     def __init__(self):
@@ -79,9 +82,60 @@ class LittleWomen:
         
         return -1
 
+    def generateSentence(self, file_name):
+        data = self.readFile(file_name)
+        all_words = data.split()
+        word_dict = defaultdict(list)
+        word_counter = Counter(all_words)
 
+        for index in range(1, len(all_words)):
 
+            prev_word = all_words[index-1]
+            curr_word = all_words[index]
+            curr_word_freq = (-word_counter[curr_word], curr_word)
+
+            word_dict[prev_word].append(curr_word_freq)
+        
+        return self.createSentence("the", 20, word_dict)
     
+    def createSentence(self, word, sentence_len, word_dict):
+        sentence = []
+        for i in range(sentence_len):
+            sentence.append(word)
+            next_word = heapq.heappop(word_dict[word])
+            word = next_word[1]
+        
+        return " ".join(sentence)+"."
+
+    def getAutocompleteSentence(self, file_name, start_sentence):
+        file_object = open(file_name, "r")
+        data = file_object.read()
+        data = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)(\s|[A-Z].*)', data)
+        self.root = TrieNode(-1)
+
+    def createTree(self, sentence):
+        temp = self.root
+
+        for char in sentence:
+            if char not in temp.children:
+                temp.children[char] = TrieNode(char)
+            
+            temp = temp.children[char]
+        
+        temp.end_of_word = True
+    
+    def allSentences(self, start_sentence):
+
+        temp = self.root
+
+        for index, char in enumerate(start_sentence):
+            pass
+
+class TrieNode:
+    def __init__(self, val):
+        self.children = {}
+        self.val = val
+        self.end_of_word = False
 
 little_women = LittleWomen()
 # print(little_women.getTotalNumberOfWords("little_women.txt"))
@@ -91,3 +145,5 @@ little_women = LittleWomen()
 # print(little_women.get20LeastFrequentWords("little_women.txt"))
 # print(little_women.getFrequencyOfWord("little_women.txt", "meg"))
 # print(little_women.getChapterQuoteAppears("little_women.txt", "If that's the way he's going to grow up, I wish he'd stay a boy"))
+# print(little_women.generateSentence("little_women.txt"))
+print(little_women.getAutocompleteSentence("little_women.txt", "Hello"))
