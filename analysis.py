7b7988552d52928315e9224728cc7ee16a67cfaa
paridhikhaitan <paridhi4699@gmail.com>
2020-07-21 04:10:36 +0530

    added comments

--- analysis.py
+++ analysis.py
@@ -10,7 +10,7 @@ import nltk
 
 # Uncomment this if you need to download nltk
 '''
-I tried to use both nltk which is a Python library and 
+I tried to use both nltk which is a Python library and
 Regex to see the difference in how well they clear strings
 '''
 # import ssl
@@ -26,25 +26,47 @@ Regex to see the difference in how well they clear strings
 
 from nltk import sent_tokenize
 
+
 class LittleWomen:
     def __init__(self):
         self.remove = str.maketrans(dict.fromkeys(string.punctuation))
 
+    # @param : file_name - name of the book
+    # @return : the whole book as a string
+    #
+    # Function opens an external file and reads it
     def readFile(self, file_name):
         file_obj = open(file_name, "r")
         data = file_obj.read().translate(self.remove)
         file_obj.close()
         return data
 
+    # @param : file_name - name of the book
+    # @return : Total number of words in the book
+    #
+    # Function converts the book into an array of words
+    # and returns the length of the array
     def getTotalNumberOfWords(self, file_name):
         data = self.readFile(file_name)
         total_words = data.lower().split()
         return len(total_words)
 
+    # @param : file_name - name of the book
+    # @return : Unique words in the book
+    #
+    # Stores all the words from the book in a set
+    # which eliminates duplicate values
+
     def getTotalUniqueWords(self, file_name):
         data = self.readFile(file_name)
         unique_words = set(data.lower().split())
         return len(unique_words)
+
+    # @param : file_name - name of the book
+    #          interesting - flag to indicate if searching for interesting words
+    # @return : Heap from the given word array
+    #
+    # Helper function that creates a word heap in O(n) time using heapify
 
     def createWordHeap(self, file_name, interesting):
         data = self.readFile(file_name)
@@ -53,6 +75,7 @@ class LittleWomen:
         if interesting:
             common_file = open("100_common.txt", "r")
             common_words = set(common_file.read().lower().split())
+            common_file.close()
             heap = []
             for key, value in word_frequency.items():
                 if key not in common_words:
@@ -63,33 +86,60 @@ class LittleWomen:
         heapq.heapify(heap)
         return heap
 
+    # @param : file_name - name of the file
+    # @return : Array of the most frequent words
+    #
+    # Creates a heap from all the words and returns the 20 most frequent ones
     def get20MostFrequentWords(self, file_name):
         NUMBER_OF_ELEMENTS = 20
         heap = self.createWordHeap(file_name, False)
         return [[element[1], element[0]] for element in heapq.nlargest(NUMBER_OF_ELEMENTS, heap)]
 
+    # @param : file_name - name of the file
+    # @return : Array of the most interesting frequent words
+    #
+    # Creates a heap from all the words and uses the list of 100_common
+    # words to eliminate uninteresting ones
     def get20MostInterestingFrequentWords(self, file_name):
         NUMBER_OF_ELEMENTS = 20
         heap = self.createWordHeap(file_name, True)
         return [[element[1], element[0]] for element in heapq.nlargest(NUMBER_OF_ELEMENTS, heap)]
 
+    # @param : file_name - name of the file
+    # @return : Array of the least frequent words
+    #
+    # Creates a heap from all the words and returns the 20 least frequent ones
     def get20LeastFrequentWords(self, file_name):
         NUMBER_OF_ELEMENTS = 20
         heap = self.createWordHeap(file_name, False)
         return [[element[1], element[0]] for element in heapq.nsmallest(NUMBER_OF_ELEMENTS, heap)]
 
+    # @param : file_name - name of the file
+    #           word - to search for frequency
+    # @return : Array of occurrence of a word by chapter
+    #
+    # Splits the data based on "CHAPTER" and then uses a dictionary to
+    # find the frequency of words in that given chapter and appends
+    # to return
     def getFrequencyOfWord(self, file_name, word):
         word = word.lower()
         data = self.readFile(file_name)
         chapter_split = data.split("CHAPTER")
-        freq_of_word = [0]*len(chapter_split)
+        freq_of_word = [0]*(len(chapter_split)-1)
         for index, chapter in enumerate(chapter_split):
             all_words = chapter.lower().split()
             words_counter = Counter(all_words)
-            freq_of_word[index] = words_counter[word]
+            freq_of_word[index-1] = words_counter[word]
 
         return freq_of_word
 
+    # @param : file_name - name of the file
+    #           quote - to search for in the chapters
+    # @return : Chapter the quote is found in
+    #           -1 if quote is not found
+    #
+    # Splits the data based on chapter and searches for the
+    # quote in each chapter
     def getChapterQuoteAppears(self, file_name, quote):
         quote = quote.translate(self.remove)
         data = self.readFile(file_name)
@@ -101,6 +151,11 @@ class LittleWomen:
 
         return -1
 
+    # @param : file_name - name of the file
+    # @return : String of 20 words in authors style
+    #
+    # Creates a mapping of current_word to (previous_word, occurrence)
+    # And uses heap to pop out the word that is most common
     def generateSentence(self, file_name):
         data = self.readFile(file_name)
         all_words = data.lower().split()
@@ -117,18 +172,37 @@ class LittleWomen:
 
         return self.createSentence("the", 20, word_dict)
 
+    # @param : file_name - name of the file
+    # @return : String of 20 words in authors style
+    #
+    # Helper function to create heap and pop from it
+    # and sets the next word based on previous word
     def createSentence(self, word, sentence_len, word_dict):
         sentence = []
+
+        common_file = open("100_common.txt", "r")
+        common_words = set(common_file.read().lower().split())
+        common_file.close()
+
         for i in range(sentence_len):
             sentence.append(word)
-            next_word = heapq.heappop(word_dict[word])
+            temp = word_dict[word]
+            next_word = heapq.heappop(temp)
             word = next_word[1]
 
         return " ".join(sentence)+"."
 
+    # @param : file_name - name of the file
+    #           start_sentence - Starting quote 
+    # @return : List of all sentences in the book
+    #
+    # Uses nltk to split the data into sentences.
+    # Creates a Trie for all the sentences
+    # Finds each autocomplete sentence and pushes it to the array
     def getAutocompleteSentence(self, file_name, start_sentence):
         file_object = open(file_name, "r")
         data = file_object.read()
+        # From nltk used to convert the data into sentences
         data = sent_tokenize(data)
         self.root = TrieNode(-1)
 
@@ -139,6 +213,10 @@ class LittleWomen:
 
         return self.allSentences(start_sentence)
 
+    # @param : sentence - To be added to the Trie
+    # @return : None
+    #
+    # Adds each sentence to the Trie
     def createTree(self, sentence):
         temp = self.root
 
@@ -149,6 +227,11 @@ class LittleWomen:
 
         temp.end_of_word = True
 
+    # @param : start_sentence - Starting quote 
+    # @return : Array of all the sentences
+    #           String if no starting quote
+    #
+    # Helper function to find all the sentences in the Trie
     def allSentences(self, start_sentence):
 
         def getTree():
@@ -179,7 +262,7 @@ class LittleWomen:
         return "No sentences that start with that!"
         
 
-
+#Trie Class
 class TrieNode:
     def __init__(self, val):
         self.children = {}
@@ -188,12 +271,12 @@ class TrieNode:
 
 
 little_women = LittleWomen()
-# print(little_women.getTotalNumberOfWords("little_women.txt"))
-# print(little_women.getTotalUniqueWords("little_women.txt"))
-# print(little_women.get20MostFrequentWords("little_women.txt"))
-# print(little_women.get20MostInterestingFrequentWords("little_women.txt"))
-# print(little_women.get20LeastFrequentWords("little_women.txt"))
-# print(little_women.getFrequencyOfWord("little_women.txt", "meg"))
-# print(little_women.getChapterQuoteAppears("little_women.txt", "If that's the way he's going to grow up, I wish he'd stay a boy"))
-# print(little_women.generateSentence("little_women.txt"))
-print(little_women.getAutocompleteSentence("little_women.txt", "Here it is, and"))
+print(little_women.getTotalNumberOfWords("little_women.txt"))
+print(little_women.getTotalUniqueWords("little_women.txt"))
+print(little_women.get20MostFrequentWords("little_women.txt"))
+print(little_women.get20MostInterestingFrequentWords("little_women.txt"))
+print(little_women.get20LeastFrequentWords("little_women.txt"))
+print(little_women.getFrequencyOfWord("little_women.txt", "Meg"))
+print(little_women.getChapterQuoteAppears("little_women.txt", "If that's the way he's going to grow up, I wish he'd stay a boy"))
+print(little_women.generateSentence("little_women.txt"))
+print(little_women.getAutocompleteSentence("little_women.txt", "I can't get"))
